f68428c43c6f30ecefedff8464c6c8332f8c65fe
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-08-16 21:53:09 +0530

    Create sum_digits_untilone2.java

---
+++ sum_digits_untilone2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n/10>0){
+            int sum = 0;
+            while(n>0){
+                sum+=n%10;
+                n/=10;
+            }
+            n = sum;
+        }
+        System.out.println(n);
+    }
+}
